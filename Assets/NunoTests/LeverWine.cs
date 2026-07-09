b479d8d277197ae8d58f169c585f7810fc1436fa
NunoAlex221 <nft.mega@gmail.com>
2026-07-09 18:10:01 +0100

    01 FloorFix, Scene Action Implement, New 05_D + Bake, Wine Lever Implement

---
+++ Assets/NunoTests/LeverWine.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class LeverWine : MonoBehaviour
+{
+    public GameObject timelineObject;
+    public PlayableDirector timeline;
+    private float rotationThreshold = 0.5;
+
+    float lastY;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        lastY = transform.localEulerAngles.y;
+        timelineObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float currentY = transform.localEulerAngles.y;
+        bool isRotating = Mathf.Abs(Mathf.DeltaAngle(lastY, currentY)) > rotationThreshold;
+
+        if (isRotating)
+        {
+            if (!timelineObject.activeSelf)
+                timelineObject.SetActive(true);
+
+            if (timeline.state != PlayState.Playing)
+                timeline.Play();
+        }
+        else
+        {
+            if (timelineObject.activeSelf)
+            {
+                timeline.Stop();
+                timelineObject.SetActive(false);
+            }
+        }
+
+        lastY = currentY;
+    }
+}
